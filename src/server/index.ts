2da0160c9890b51174808058e5f55430ad354065
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-10-08 17:44:48 +0200

    Fix build

--- src/server/index.ts
+++ src/server/index.ts
@@ -43,7 +43,7 @@ app.use(rateLimiter)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './'))
 
-app.use(serveStatic(path.join(__dirname, '../build-frontend/static'), {
+app.use('/static', serveStatic(path.join(__dirname, '../../../build-frontend/static'), {
   cacheControl: true,
   maxAge: 31536000,
 }));
@@ -113,7 +113,7 @@ app.get('/articles/:articleId/audiofiles/:audiofileId', async (req: Request, res
     }
 
     // Render the embed page with the article API data inside, so React can use that data to render the player
-    const embedPageRendered = await ejs.renderFile(path.join(__dirname, '../../build-frontend/index.ejs'), {
+    const embedPageRendered = await ejs.renderFile(path.join(__dirname, '../../../build-frontend/index.ejs'), {
       title: article.title,
       article: JSON.stringify(article),
       audiofile: JSON.stringify(audiofile),
