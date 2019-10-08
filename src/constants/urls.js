ecf3059d36eda88773c41d0a8730238d069b37f3
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-10-08 10:35:06 +0200

    Remove js build files from src

--- src/constants/urls.js
+++
@@ -1,4 +0,0 @@
-export var URL_APPLE_APP_STORE = 'https://apps.apple.com/app/playpost/id1460663960?ls=1';
-export var URL_GOOGLE_PLAY_STORE = 'https://play.google.com/store/apps/details?id=com.aardwegmedia.playpost';
-export var URL_PLAYPOST_WEBSITE = 'https://playpost.app';
-//# sourceMappingURL=urls.js.map
--- src/utils/analytics.js
+++
@@ -1,6 +0,0 @@
-export var trackEvent = function (name, articleId, data) {
-    return new Promise(function (resolve, reject) {
-        return resolve(console.log('Should track:', name, articleId, data));
-    });
-};
-//# sourceMappingURL=analytics.js.map
