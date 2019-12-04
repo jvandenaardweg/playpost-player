4aaec1b4499cf4165f92515d8dd3064d27b77e01
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-12-04 09:08:04 +0100

    Cache pubsub client for faster publishes

--- src/server/pubsub/events.ts
+++ src/server/pubsub/events.ts
@@ -19,25 +19,34 @@ export interface PlayerEvent {
   city: string | null;
 }
 
+const pubsub = new PubSub(getGoogleCloudCredentials());
+
+if (!GOOGLE_CLOUD_PUBSUB_TOPIC_NAME) {
+  const errorMessage = 'Required env variable "GOOGLE_CLOUD_PUBSUB_TOPIC_NAME" not set. Please add it.';
+  logger.error(errorMessage);
+  throw new Error(errorMessage);
+}
+
+// Make the pubsub client here, so we can re-use it
+// Resulting in faster publishes
+const pubsubClient = pubsub.topic(GOOGLE_CLOUD_PUBSUB_TOPIC_NAME, {
+  batching: {
+    maxMessages: 1,
+    maxMilliseconds: 1
+  }
+})
+
 export async function publishEvent(event: PlayerEvent) {
   const loggerPrefix = 'Publish Event to Google Cloud PubSub:';
 
   try {
-    const pubsub = new PubSub(getGoogleCloudCredentials());
-
-    if (!GOOGLE_CLOUD_PUBSUB_TOPIC_NAME) {
-      const errorMessage = 'Required env variable "GOOGLE_CLOUD_PUBSUB_TOPIC_NAME" not set. Please add it.';
-      logger.error(loggerPrefix, errorMessage);
-      throw new Error(errorMessage);
-    }
-
     const buffer = Buffer.from(
       JSON.stringify(event)
     );
 
     logger.info(loggerPrefix, 'Publishing...', event);
 
-    const result = await pubsub.topic(GOOGLE_CLOUD_PUBSUB_TOPIC_NAME).publish(buffer);
+    const result = await pubsubClient.publish(buffer);
 
     logger.info(loggerPrefix, 'Published to:', GOOGLE_CLOUD_PUBSUB_TOPIC_NAME);
 
