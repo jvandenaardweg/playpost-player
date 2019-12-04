77b37b776e6d02dde73589ebc5d3e9871a1e29b5
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-12-04 08:23:20 +0100

    Only enable sentry on production

--- src/server/sentry.ts
+++ src/server/sentry.ts
@@ -5,7 +5,7 @@ import * as Sentry from '@sentry/node';
 
 Sentry.init({
   dsn: 'https://3774ad85250645f18efe40450f42bb0e@sentry.io/1844161',
-  // enabled: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test', // Do not run on your local machine
+  enabled: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test', // Do not run on your local machine
   environment: process.env.NODE_ENV,
   integrations: [
     new Integrations.RewriteFrames({
